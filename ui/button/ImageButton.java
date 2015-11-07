cdde6652f4d1b04aaafa16d7099b03681a0efd13
Rubixdoed <rubixdoed@gmail.com>
2015-11-06 20:19:03 -0700

    Update ImageButton.java

--- ui/button/ImageButton.java
+++ ui/button/ImageButton.java
@@ -1,5 +1,28 @@
-import ui.button;
+package ui.button;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 public class ImageButton extends Button{
+	
+	private BufferedImage[] images;
+	
+	public ImageButton(int x, int y, int w, int h, String action, BufferedImage[] images) {
+		super(x, y, w, h, action);
+		this.images = images;
+	}
+
+	@Override
+	public void render(Graphics g) {
+		if( active ){
+			if( mouseOver ){
+				g.drawImage(images[0],x,y,null);
+			} else {
+				g.drawImage(images[1],x,y,null);
+			}
+		} else {
+			g.drawImage(images[2],x,y,null);
+		}
+	}
 	
 }
