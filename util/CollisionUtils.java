49c071157fc579d8eea5a28e6a97ba390cf81e12
Rubixdoed <rubixdoed@gmail.com>
2015-11-06 20:53:10 -0700

    Added pointInRectangle( double, double, Rectangle )

--- util/CollisionUtils.java
+++ util/CollisionUtils.java
@@ -9,6 +9,10 @@ public class CollisionUtils{
 		return px >= x && px < x+w && py >= y && py < y+h;
 	}
 	
+	public static boolean pointInRectangle(double x, double y, Rectangle rectangle){
+		return x >= rectangle.getX() && x < rectangle.getX() + rectangle.getW() && y >= rectangle.getY() && y < rectangle.getY() + rectangle.getH();
+	}
+	
 	public static boolean pointInRectangle(Point2D point, Rectangle rectangle){
 		return point.getX() >= rectangle.getX() && point.getX() < rectangle.getX() + rectangle.getW() && point.getY() >= rectangle.getY() && point.getY() < rectangle.getY() + rectangle.getH();
 	}
