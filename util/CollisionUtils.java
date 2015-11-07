909c9e79bce6e60c1459991ab87c53ea11f0739d
Rubixdoed <rubixdoed@gmail.com>
2015-11-06 20:50:03 -0700

    Multiple Changes
    +added pointInRectangle( Point2D, Rectangle )
    
    ~changed precision of pointInRectangle() parameters ( int -> double )
    
    *fixed file extension ( -> .java )

---
+++ util/CollisionUtils.java
@@ -0,0 +1,16 @@
+package util;
+
+import util.shape.Rectangle;
+import util.shape.Point2D;
+
+public class CollisionUtils{
+	
+	public static boolean pointInRectangle(double px, double py, double x, double y, double w, double h){
+		return px >= x && px < x+w && py >= y && py < y+h;
+	}
+	
+	public static boolean pointInRectangle(Point2D point, Rectangle rectangle){
+		return point.getX() >= rectangle.getX() && point.getX() < rectangle.getX() + rectangle.getW() && point.getY() >= rectangle.getY() && point.getY() < rectangle.getY() + rectangle.getH();
+	}
+	
+}
